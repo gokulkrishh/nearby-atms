893453febda2042eb1f195c77a94fa0938a1e639
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-11-21 10:58:24 +0530

    Added map layout with nearby atms

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,28 +4,72 @@ import config from '../../config.json';
 import style from './style';
 
 export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.getLocation = this.getLocation.bind(this);
+	}
+
 	componentDidMount(event) {
 		GoogleMapsLoader.KEY = config.mapAPIKey;
-		const EIFFEL_TOWER_POSITION = {
-		  lat: 48.858608,
-		  lng: 2.294471
-		};
-
-		const options = {
-			center: EIFFEL_TOWER_POSITION,
-			zoom: 17,
-			fullscreenControl: true, 
-			zoomControl: false,
-			mapTypeControl: false,
-			signInControl: true
-		};
-		
-
-		GoogleMapsLoader.load((google) => {
-    	var maps = new google.maps.Map(document.querySelector('#map'), options);
-		});
-
+		GoogleMapsLoader.LIBRARIES = ['geometry', 'places'];
+		GoogleMapsLoader.REGION = 'IN';
 		console.log(GoogleMapsLoader)
+		this.getLocation();
+	}
+
+	getLocation() {
+		navigator.geolocation.getCurrentPosition((position) => { 
+			const myLatLng = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			GoogleMapsLoader.load((google) => {
+				const options = {
+					center: myLatLng,
+					zoom: 17,
+					fullscreenControl: true, 
+					backgroundColor: '#fafafa',
+					zoomControl: false,
+					mapTypeControl: false,
+					signInControl: true
+				};
+    		
+    		var map = new google.maps.Map(document.getElementById('map'), options);
+
+    		var marker = new google.maps.Marker({
+          position: myLatLng,
+          label: 'You',
+          animation: google.maps.Animation.DROP,
+          map: map
+        });
+
+        var request = {
+			    location: myLatLng,
+			    radius: 250,
+			    types: ['atm']
+			  };
+
+        var service = new google.maps.places.PlacesService(map);
+
+  			service.nearbySearch(request, (results, status) => {
+  				if (status == google.maps.places.PlacesServiceStatus.OK) {
+				    for (var i = 0; i < results.length; i++) {
+				      createMarker(results[i]);
+				    }
+				  }
+  			});
+
+  			function createMarker(place) {
+  				console.log(place)
+				  var marker = new google.maps.Marker({
+				    map: map,
+				    position: place.geometry.location,
+				    animation: google.maps.Animation.BOUNCE
+				  });
+  			}
+			});
+		});
 	}
 
 	componentWillUnmount() {
@@ -38,12 +82,17 @@ export default class Home extends Component {
 		return (
 			<div class={style.home}>
 				<div id="map" class={style.map}>
-					Loading map...
+					<p class="loading">Loading map...</p>
 				</div>
 
 				<div class={style.actions}>
-					<button></button>
-					<p>Locate Me</p>
+					<button onClick={this.getLocation}>
+						<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					    <path d="M0 0h24v24H0z" fill="none"/>
+					    <path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/>
+						</svg>
+					</button>
+					<p>Locate ATM Nearby</p>
 				</div>
 			</div>
 		);
